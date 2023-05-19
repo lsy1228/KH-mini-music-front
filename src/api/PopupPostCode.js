651c878ddd017027cc33f53b74db02310164f045
fearofgod0001 <dlxo8888@gmail.com>
2023-05-19 11:29:34 +0900

    0519_포스트코드 닫기

--- src/api/PopupPostCode.js
+++ src/api/PopupPostCode.js
@@ -1,13 +1,23 @@
 import React,{useContext} from 'react';
 import DaumPostcode from "react-daum-postcode";
 import { UserContext } from '../context/UserInfo';
+import styled from "styled-components";
 
   //yarn add react-daum
 
-const PopupPostCode = () => {
+
+  const Button = styled.button`
+        display: flex;
+        top: 10%;
+        width: 80px;
+        height: 40px;
+        padding: 7px;
+  `
+const PopupPostCode = (props) => {
 	  // 우편번호 검색 후 주소 클릭 시 실행될 함수, data callback 용
     const context = useContext(UserContext);
     const {setAddr} = context; 
+    const {changePage}=props;
 
     const handlePostCode = (data) => {
         let fullAddress = data.address;
@@ -25,12 +35,15 @@ const PopupPostCode = () => {
         console.log(data);
         console.log(fullAddress);
         console.log(data.zonecode);
-        setAddr(fullAddress);    
+        setAddr(fullAddress);
+        props.onClose();
     };
   
     return(
+      <div>
       <DaumPostcode  onComplete={handlePostCode} />
-    );
-};
+      </div>
+  )
+}
  
 export default PopupPostCode;
